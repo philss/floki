2a69a0720ac6708b06037d25557ec9aa50898d54
Mohamed Seleem <seleem1337@gmail.com>
2017-10-21 16:14:25 +0200

    Refactor finding children nodes out of 'node_position/2' into it's own 'get_children_nodes_ids/2'

--- lib/floki/selector/pseudo_class.ex
+++ lib/floki/selector/pseudo_class.ex
@@ -52,15 +52,18 @@ defmodule Floki.Selector.PseudoClass do
   end
 
   defp node_position(tree, html_node) do
-    parent_node = Map.get(tree.nodes, html_node.parent_node_id)
-    children_nodes_ids = parent_node.children_nodes_ids
-                         |> Enum.reverse
-                         |> filter_only_html_nodes(tree.nodes)
-                         |> Enum.with_index(1)
-
+    children_nodes_ids = get_children_nodes_ids(tree, html_node.parent_node_id)
     {_node_id, position} = Enum.find(children_nodes_ids, fn({id, _}) -> id == html_node.node_id end)
 
     position
+  end
+
+  defp get_children_nodes_ids(tree, parent_node_id) do
+    parent_node = Map.get(tree.nodes, parent_node_id)
+    parent_node.children_nodes_ids
+    |> Enum.reverse
+    |> filter_only_html_nodes(tree.nodes)
+    |> Enum.with_index(1)
   end
 
   defp filter_only_html_nodes(ids, nodes) do
