78657f919cf68808894830b2332b3813bf74aede
Philip Sampaio Silva <philip.sampaio@gmail.com>
2019-05-12 19:53:27 -0300

    Add the html5lib-tests as a submodule
    It is needed to better test the specs of HTML5.

--- test/floki/html/tokenizer_test.exs
+++ test/floki/html/tokenizer_test.exs
@@ -2,5 +2,5 @@ defmodule Floki.HTML.TokenizerTest do
   use ExUnit.Case, async: true
   use TokenizerTestLoader
 
-  load_tests_from_file("./test/fixtures/test1.json")
+  load_tests_from_file("./test/html5lib-tests/tokenizer/test1.test")
 end
