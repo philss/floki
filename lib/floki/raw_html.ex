34f01de6626075f82cb36e38c0fe434dd9f2bf03
Tan Huynh <danhuynhdev@gmail.com>
2019-10-06 17:51:40 +0700

    Escape problematic characters when encoding to html

--- lib/floki/raw_html.ex
+++ lib/floki/raw_html.ex
@@ -34,7 +34,8 @@ defmodule Floki.RawHTML do
   end
 
   defp build_raw_html([], html, _encoder), do: html
-  defp build_raw_html(string, _html, encoder) when is_binary(string), do: encoder.(string)
+  defp build_raw_html(string, _html, encoder) when is_binary(string),
+    do: html_escape_text(encoder.(string))
 
   defp build_raw_html(tuple, html, encoder) when is_tuple(tuple),
     do: build_raw_html([tuple], html, encoder)
@@ -88,11 +89,7 @@ defmodule Floki.RawHTML do
   end
 
   defp build_attrs({attr, <<value::binary>>}, attrs) do
-    if String.contains?(value, "\"") do
-      ~s(#{attrs} #{attr}='#{value}')
-    else
-      ~s(#{attrs} #{attr}="#{value}")
-    end
+    "#{attrs} #{attr}=\"#{html_escape_attribute_value(value)}\""
   end
 
   defp build_attrs(<<attr::binary>>, attrs), do: "#{attrs} #{attr}"
@@ -116,4 +113,38 @@ defmodule Floki.RawHTML do
       & &1
     end
   end
+
+  # html_escape
+
+  def html_escape_attribute_value(attribute_value) do
+    html_escape_chars(attribute_value, ["&", "\""])
+  end
+
+  def html_escape_text(text) do
+    html_escape_chars(text, ["&", "<", ">"])
+  end
+
+  defp html_escape_chars(subject, escaped_chars) do
+    matches = :binary.matches(subject, escaped_chars)
+
+    subject
+    |> do_replace(matches, &html_escape_char/1, 0)
+    |> IO.iodata_to_binary()
+  end
+
+  defp do_replace(subject, [], _, n) do
+    [binary_part(subject, n, byte_size(subject) - n)]
+  end
+
+  defp do_replace(subject, [{start, length} | matches], replacement, n) do
+    prefix = binary_part(subject, n, start - n)
+    middle = replacement.(binary_part(subject, start, length))
+    [prefix, middle | do_replace(subject, matches, replacement, start + length)]
+  end
+
+  defp html_escape_char("<"), do: "&lt;"
+  defp html_escape_char(">"), do: "&gt;"
+  defp html_escape_char("&"), do: "&amp;"
+  defp html_escape_char("\""), do: "&quot;"
+  defp html_escape_char("'"), do: "&#39;"
 end
