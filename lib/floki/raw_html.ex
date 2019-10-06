43b280e6889fdc6911e3beda9710758cdc6f57ae
Tan Huynh <danhuynhdev@gmail.com>
2019-10-06 18:59:19 +0700

    Use String.replace instead of homebake one

--- lib/floki/raw_html.ex
+++ lib/floki/raw_html.ex
@@ -117,29 +117,15 @@ defmodule Floki.RawHTML do
   # html_escape
 
   def html_escape_attribute_value(attribute_value) do
-    html_escape_chars(attribute_value, ["&", "\""])
+    html_escape_chars(attribute_value, ~r/&|"/)
   end
 
   def html_escape_text(text) do
-    html_escape_chars(text, ["&", "<", ">"])
+    html_escape_chars(text, ~r/&|<|>/)
   end
 
   defp html_escape_chars(subject, escaped_chars) do
-    matches = :binary.matches(subject, escaped_chars)
-
-    subject
-    |> do_replace(matches, &html_escape_char/1, 0)
-    |> IO.iodata_to_binary()
-  end
-
-  defp do_replace(subject, [], _, n) do
-    [binary_part(subject, n, byte_size(subject) - n)]
-  end
-
-  defp do_replace(subject, [{start, length} | matches], replacement, n) do
-    prefix = binary_part(subject, n, start - n)
-    middle = replacement.(binary_part(subject, start, length))
-    [prefix, middle | do_replace(subject, matches, replacement, start + length)]
+    String.replace(subject, escaped_chars, &html_escape_char/1)
   end
 
   defp html_escape_char("<"), do: "&lt;"
