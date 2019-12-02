2cd6c1e8690efd2a21ae36e18e57e2d515dc4549
rinpatch <rinpatch@sdf.org>
2019-12-02 23:29:51 +0300

    Add fast_html parser

--- mix.exs
+++ mix.exs
@@ -13,6 +13,7 @@ defmodule Floki.Mixfile do
       elixir: "~> 1.5",
       package: package(),
       deps: deps(),
+      aliases: aliases(),
       source_url: "https://github.com/philss/floki",
       docs: [extras: ["README.md"], main: "Floki", assets: "assets"]
     ]
@@ -28,8 +29,50 @@ defmodule Floki.Mixfile do
       {:earmark, "~> 1.2", only: :dev},
       {:ex_doc, "~> 0.18", only: :dev},
       {:credo, ">= 0.0.0", only: [:dev, :test]},
-      {:inch_ex, ">= 0.0.0", only: :docs}
+      {:inch_ex, ">= 0.0.0", only: :docs},
+      {:html5ever, ">= 0.0.0", optional: true, only: [:dev, :test]},
+      {:fast_html, ">= 0.0.0", optional: true, only: [:dev, :test]},
+      {:mochiweb, ">= 0.0.0", optional: true, only: [:dev, :test]}
     ]
+  end
+
+  defp aliases do
+    parsers = get_parsers()
+
+    {aliases, cli_names} =
+      Enum.map_reduce(parsers, [], fn parser, acc ->
+        cli_name =
+          parser
+          |> Module.split()
+          |> List.last()
+          |> Macro.underscore()
+
+        {{:"test.#{cli_name}", &test_with_parser(parser, &1)}, [cli_name | acc]}
+      end)
+
+    aliases
+    |> Keyword.put(:test, &test_with_parser(cli_names, &1))
+  end
+
+  # Hardcoded because we can't load the floki application and get the module list at this point.
+  defp get_parsers() do
+    [Floki.HTMLParser.Mochiweb, Floki.HTMLParser.FastHtml, Floki.HTMLParser.Html5ever]
+  end
+
+  # Hack: If Mix.Task.Test.run is called the second time, it won't run any tests
+  defp test_with_parser(parser_cli_names, args) when is_list(parser_cli_names) do
+    Enum.each(parser_cli_names, fn cli_name ->
+      Mix.shell().cmd("mix test.#{cli_name} --color #{Enum.join(args, " ")}",
+        env: [{"MIX_ENV", "test"}]
+      )
+    end)
+  end
+
+  defp test_with_parser(parser, args) do
+    Mix.shell().info("Running tests with #{parser}")
+    Application.put_env(:floki, :html_parser, parser, persistent: true)
+    Mix.env(:test)
+    Mix.Tasks.Test.run(args)
   end
 
   defp package do
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,7 @@
+# fast_html uses a C-Node worker for parsing, so starting the application
+# is necessary for it to work
+if Application.get_env(:floki, :html_parser) == Floki.HTMLParser.FastHtml do
+  Application.ensure_all_started(:fast_html)
+end
+
 ExUnit.start()
