d2d8ac45e5d33806b70009c241538a86c7431199
Eunbin Jeong <me@dalgona.dev>
2020-01-23 22:41:28 +0900

    Add Floki.Traversal.traverse_and_update/3

--- lib/floki/traversal.ex
+++ lib/floki/traversal.ex
@@ -22,4 +22,30 @@ defmodule Floki.Traversal do
   def traverse_and_update({:comment, children}, fun), do: fun.({:comment, children})
 
   def traverse_and_update(doctype = {:doctype, _, _, _}, fun), do: fun.(doctype)
+
+  def traverse_and_update(text, acc, _fun) when is_binary(text), do: {text, acc}
+
+  def traverse_and_update([head | tail], acc, fun) do
+    case traverse_and_update(head, acc, fun) do
+      {nil, new_acc} ->
+        traverse_and_update(tail, new_acc, fun)
+
+      {mapped_head, new_acc} ->
+        {mapped_tail, new_acc2} = traverse_and_update(tail, new_acc, fun)
+        {[mapped_head | mapped_tail], new_acc2}
+    end
+  end
+
+  def traverse_and_update([], acc, _fun), do: {[], acc}
+
+  def traverse_and_update(xml_tag = {:pi, _, _}, acc, fun), do: fun.(xml_tag, acc)
+
+  def traverse_and_update({elem, attrs, children}, acc, fun) do
+    {mapped_children, new_acc} = traverse_and_update(children, acc, fun)
+    fun.({elem, attrs, mapped_children}, new_acc)
+  end
+
+  def traverse_and_update({:comment, children}, acc, fun), do: fun.({:comment, children}, acc)
+
+  def traverse_and_update(doctype = {:doctype, _, _, _}, acc, fun), do: fun.(doctype, acc)
 end
