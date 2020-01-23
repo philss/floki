4f4517404bda72e5fc43e71a4df20364f8c79911
Eunbin Jeong <me@dalgona.dev>
2020-01-23 22:41:28 +0900

    Add Floki.Traversal.traverse_and_update/3
    This function enables updating the tree with an accumulator. It is
    useful in cases where you need to keep track of changes.
    
    With this function, we can have a functional approach to
    traversing and manipulating HTML trees with a state.
    
    This commit also improves `Floki.Traversal`, and improves the docs for
    `Floki.traverse_and_update/2` (the one without accumulator).
    
    Closes https://github.com/philss/floki/issues/246

--- lib/floki/traversal.ex
+++ lib/floki/traversal.ex
@@ -1,7 +1,12 @@
 defmodule Floki.Traversal do
   @moduledoc false
 
+  def traverse_and_update(html_node, fun)
+  def traverse_and_update([], _fun), do: []
   def traverse_and_update(text, _fun) when is_binary(text), do: text
+  def traverse_and_update(xml_tag = {:pi, _, _}, fun), do: fun.(xml_tag)
+  def traverse_and_update({:comment, children}, fun), do: fun.({:comment, children})
+  def traverse_and_update(doctype = {:doctype, _, _, _}, fun), do: fun.(doctype)
 
   def traverse_and_update([head | tail], fun) do
     case traverse_and_update(head, fun) do
@@ -10,16 +15,31 @@ defmodule Floki.Traversal do
     end
   end
 
-  def traverse_and_update([], _fun), do: []
-
-  def traverse_and_update(xml_tag = {:pi, _, _}, fun), do: fun.(xml_tag)
-
   def traverse_and_update({elem, attrs, children}, fun) do
     mapped_children = traverse_and_update(children, fun)
     fun.({elem, attrs, mapped_children})
   end
 
-  def traverse_and_update({:comment, children}, fun), do: fun.({:comment, children})
+  def traverse_and_update(html_node, acc, fun)
+  def traverse_and_update([], acc, _fun), do: {[], acc}
+  def traverse_and_update(text, acc, _fun) when is_binary(text), do: {text, acc}
+  def traverse_and_update(xml_tag = {:pi, _, _}, acc, fun), do: fun.(xml_tag, acc)
+  def traverse_and_update({:comment, children}, acc, fun), do: fun.({:comment, children}, acc)
+  def traverse_and_update(doctype = {:doctype, _, _, _}, acc, fun), do: fun.(doctype, acc)
 
-  def traverse_and_update(doctype = {:doctype, _, _, _}, fun), do: fun.(doctype)
+  def traverse_and_update([head | tail], acc, fun) do
+    case traverse_and_update(head, acc, fun) do
+      {nil, new_acc} ->
+        traverse_and_update(tail, new_acc, fun)
+
+      {mapped_head, new_acc} ->
+        {mapped_tail, new_acc2} = traverse_and_update(tail, new_acc, fun)
+        {[mapped_head | mapped_tail], new_acc2}
+    end
+  end
+
+  def traverse_and_update({elem, attrs, children}, acc, fun) do
+    {mapped_children, new_acc} = traverse_and_update(children, acc, fun)
+    fun.({elem, attrs, mapped_children}, new_acc)
+  end
 end
--- test/floki/traversal_test.exs
+++ test/floki/traversal_test.exs
@@ -76,4 +76,87 @@ defmodule Floki.TraversalTest do
              ]
     end
   end
+
+  describe "traverse_and_update/3" do
+    test "with string returns string and the original accumulator value" do
+      assert Floki.traverse_and_update("hello", 0, fn tag, acc ->
+               {tag, acc + 1}
+             end) == {"hello", 0}
+    end
+
+    test "with div and identity function returns div" do
+      html = {"div", [], ["hello"]}
+
+      assert Floki.traverse_and_update(html, 0, fn tag, acc ->
+               {tag, acc + 1}
+             end) == {html, 1}
+    end
+
+    test "with div and div->p function returns p" do
+      html = {"div", [], ["hello"]}
+
+      assert Floki.traverse_and_update(html, 0, fn {"div", attrs, children}, acc ->
+               {{"p", attrs, children}, acc + 1}
+             end) == {{"p", [], ["hello"]}, 1}
+    end
+
+    test "with style attribute and style->src function returns src attribute" do
+      html = {"div", [{"style", "display: flex"}], ["hello"]}
+
+      assert Floki.traverse_and_update(html, 0, fn {elem, _attrs, children}, acc ->
+               {{elem, [{"src", "http://example.test"}], children}, acc + 1}
+             end) == {{"div", [{"src", "http://example.test"}], ["hello"]}, 1}
+    end
+
+    test "with text child and child replacer function returns tag with new child" do
+      html = {"div", [], ["hello"]}
+
+      assert Floki.traverse_and_update(html, 0, fn {elem, attrs, _children}, acc ->
+               {{elem, attrs, ["world"]}, acc + 1}
+             end) == {{"div", [], ["world"]}, 1}
+    end
+
+    test "with div->span and span deleter function returns div without span" do
+      html = {"div", [], [{"span", [], ["hello"]}]}
+
+      assert Floki.traverse_and_update(html, 0, fn
+               {"span", _attrs, _children}, acc -> {nil, acc + 1}
+               tag, acc -> {tag, acc + 1}
+             end) == {{"div", [], []}, 2}
+    end
+
+    test "with div,p and identity function returns div,p" do
+      html = [{"div", [], ["hello"]}, {"p", [], ["world"]}]
+
+      assert Floki.traverse_and_update(html, 0, fn tag, acc ->
+               {tag, acc + 1}
+             end) == {html, 2}
+    end
+
+    test "with comment, pi and doctype elements" do
+      html = [
+        {:doctype, "html", nil, nil},
+        {"body", [],
+         [
+           {"div", [], ["hello"]},
+           {:comment, "a comment"},
+           {:pi, "xml", []}
+         ]}
+      ]
+
+      assert Floki.traverse_and_update(html, 0, fn
+               {:comment, text}, acc -> {{"span", [], [text]}, acc + 1}
+               {:pi, "xml", _children}, acc -> {nil, acc + 1}
+               {:doctype, "html", nil, nil}, acc -> {nil, acc + 1}
+               tag, acc -> {tag, acc + 1}
+             end) ==
+               {[
+                  {"body", [],
+                   [
+                     {"div", [], ["hello"]},
+                     {"span", [], ["a comment"]}
+                   ]}
+                ], 5}
+    end
+  end
 end
