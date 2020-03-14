fa7191f323c5abbe08ec4935313fe8c2fa1c3033
Philip Sampaio Silva <philip.sampaio@gmail.com>
2020-03-14 15:11:48 -0300

    Remove module that is duplicated
    It was moved to the test/support directory.
    Note that it does not run tests individually, like test.mochiweb

--- mix.exs
+++ mix.exs
@@ -114,6 +114,6 @@ defmodule Floki.Mixfile do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
   defp elixirc_paths(_), do: ["lib"]
 end
