b2a80573af20ae657ccb96e6e85a28d14a315efc
José Valim <jose.valim@dashbit.co>
2020-06-17 21:18:38 +0200

    Remove duplication in traverse and update

--- lib/floki/traversal.ex
+++ lib/floki/traversal.ex
@@ -1,23 +1,10 @@
 defmodule Floki.Traversal do
   @moduledoc false
 
-  def traverse_and_update(html_node, fun)
-  def traverse_and_update([], _fun), do: []
-  def traverse_and_update(text, _fun) when is_binary(text), do: text
-  def traverse_and_update(xml_tag = {:pi, _, _}, fun), do: fun.(xml_tag)
-  def traverse_and_update({:comment, children}, fun), do: fun.({:comment, children})
-  def traverse_and_update(doctype = {:doctype, _, _, _}, fun), do: fun.(doctype)
-
-  def traverse_and_update([head | tail], fun) do
-    case traverse_and_update(head, fun) do
-      nil -> traverse_and_update(tail, fun)
-      mapped_head -> [mapped_head | traverse_and_update(tail, fun)]
-    end
-  end
-
-  def traverse_and_update({elem, attrs, children}, fun) do
-    mapped_children = traverse_and_update(children, fun)
-    fun.({elem, attrs, mapped_children})
+  def traverse_and_update(html_node, fun) do
+    html_node
+    |> traverse_and_update([], fn element, acc -> {fun.(element), acc} end)
+    |> elem(0)
   end
 
   def traverse_and_update(html_node, acc, fun)
