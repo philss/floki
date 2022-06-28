be2b172b6b8aa86c2755bb4c5b362171e57660cf
Philip Sampaio <philip.sampaio@gmail.com>
2022-06-28 12:09:21 -0300

    Change sponsors links - using GitHub Sponsors

--- mix.exs
+++ mix.exs
@@ -120,7 +120,7 @@ defmodule Floki.Mixfile do
       ],
       links: %{
         "Changelog" => "https://hexdocs.pm/floki/changelog.html",
-        "Sponsor" => "https://www.paypal.com/donate?business=EMDBKWVHVEB7Q&currency_code=USD",
+        "Sponsor" => "https://github.com/sponsors/philss",
         "GitHub" => @source_url
       }
     }
