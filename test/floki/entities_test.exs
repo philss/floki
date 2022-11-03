2a12e63d17d88bc9442c711f13e7532759bc3a09
Philip Sampaio <philip.sampaio@gmail.com>
2022-11-03 14:22:59 -0300

    Add tests to Entities module

---
+++ test/floki/entities_test.exs
@@ -0,0 +1,60 @@
+defmodule Floki.EntitiesTest do
+  use ExUnit.Case, async: true
+
+  alias Floki.Entities
+
+  describe "encode/1" do
+    test "encode single-quote" do
+      assert Entities.encode("'") == "&#39;"
+    end
+
+    test "encode double-quote" do
+      assert Entities.encode("\"") == "&quot;"
+    end
+
+    test "ampersand" do
+      assert Entities.encode("&") == "&amp;"
+    end
+
+    test "encode less-than sign" do
+      assert Entities.encode("<") == "&lt;"
+    end
+
+    test "encode greater-than sign" do
+      assert Entities.encode(">") == "&gt;"
+    end
+
+    test "does not encode others" do
+      assert Entities.encode("!") == "!"
+      assert Entities.encode("?") == "?"
+      assert Entities.encode("aaaa") == "aaaa"
+    end
+  end
+
+  describe "decode/1" do
+    test "decode all known entities" do
+      {:ok, json} = Jason.decode(File.read!("priv/entities.json"))
+      entities = Map.keys(json)
+
+      for entity <- entities do
+        assert {:ok, valid} = Entities.decode(entity)
+        assert String.valid?(valid)
+      end
+    end
+
+    test "decode some numeric references" do
+      entities =
+        ~w(&#x202B; &#1585; &#1602; &#1605; &#1575; &#1604; &#1607; &#1575; &#1578; &#1601; &#x202E;)
+
+      for entity <- entities do
+        assert {:ok, valid} = Entities.decode(entity)
+        assert String.valid?(valid)
+      end
+    end
+
+    test "returns not found for unknown entities" do
+      assert {:error, :not_found} = Entities.decode("&pastel;")
+      assert {:error, :not_found} = Entities.decode("&churrasco;")
+    end
+  end
+end
