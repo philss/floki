652285d863bf7781c86a8072bc216fd466459b6f
preciz <barna.kovacs@tutanota.com>
2026-03-17 11:21:03 +0100

    Optimize FlatText.get/3 memory usage and speed
    Replaced Enum.reduce/3 with an optimal tail-recursive approach in FlatText.
    
    By traversing lists manually, we avoid the overhead of the Enumerable protocol and the allocation of anonymous closures per reduction iteration.
    
    Benchmark results when running Floki.FlatText.get on a large document:
    
    ```text
    Name                     ips        average  deviation         median         99th %
    Tail recursion        3.21 M      311.16 ns ±23057.88%          50 ns         160 ns
    Enum.reduce           2.76 M      361.71 ns ±20499.20%          50 ns         180 ns
    
    Comparison:
    Tail recursion        3.21 M
    Enum.reduce           2.76 M - 1.16x slower +50.55 ns
    
    Memory usage statistics:
    
    Name              Memory usage
    Tail recursion           136 B
    Enum.reduce              208 B - 1.53x memory usage +72 B
    ```

--- lib/floki/flat_text.ex
+++ lib/floki/flat_text.ex
@@ -17,9 +17,7 @@ defmodule Floki.FlatText do
 
   def get(html_nodes, sep, include_inputs?) when is_list(html_nodes) do
     html_nodes
-    |> Enum.reduce([], fn html_node, acc ->
-      text_from_node(html_node, acc, 0, sep, include_inputs?)
-    end)
+    |> text_from_nodes([], 0, sep, include_inputs?)
     |> IO.iodata_to_binary()
   end
 
@@ -27,6 +25,12 @@ defmodule Floki.FlatText do
     html_node
     |> text_from_node([], 0, sep, include_inputs?)
     |> IO.iodata_to_binary()
+  end
+
+  defp text_from_nodes([], acc, _, _, _), do: acc
+  defp text_from_nodes([node | rest], acc, depth, sep, include_inputs?) do
+    acc = text_from_node(node, acc, depth, sep, include_inputs?)
+    text_from_nodes(rest, acc, depth, sep, include_inputs?)
   end
 
   defp text_from_node({"input", attrs, []}, acc, _, _, true) do
@@ -39,9 +43,7 @@ defmodule Floki.FlatText do
 
   defp text_from_node({_tag, _attrs, html_nodes}, acc, depth, sep, include_inputs?)
        when depth < 1 do
-    Enum.reduce(html_nodes, acc, fn html_node, acc ->
-      text_from_node(html_node, acc, depth + 1, sep, include_inputs?)
-    end)
+    text_from_nodes(html_nodes, acc, depth + 1, sep, include_inputs?)
   end
 
   defp text_from_node(text, [], _, _sep, _) when is_binary(text), do: text
